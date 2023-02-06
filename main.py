9cd3d12d2626deabb2eeab03c455d5ef4cede9b3
Kippiii <ianorzel@gmail.com>
2023-02-06 17:03:43 -0500

    Adding ability to download files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, abort, send_file
 
 from FileSystem import FileSystem
+from Config import Config
 
 app = Flask(__name__)
 
@@ -9,3 +10,10 @@ def get_file_list():
     return {
         "archives": FileSystem.get_archives(),
     }
+
+@app.route("/get/<archive_name>")
+def get_archive_by_name(archive_name: str):
+    archives: List[str] = FileSystem.get_archives()
+    if archive_name not in archives:
+        abort(404)
+    return send_file(Config.get_archives_path() / archive_name)
