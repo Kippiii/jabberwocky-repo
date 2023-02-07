9e597b9179df2e2a8f09114c0b2ae696ca2570fd
Kippiii <ianorzel@gmail.com>
2023-02-06 19:14:58 -0500

    Getting the archive path from a config file

--- Config.py
+++ Config.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 class Config:
@@ -9,4 +10,8 @@ class Config:
         """
         Returns the path to where archives are stored
         """
-        return Path("/home/fcorp/Downloads/") # TODO Fix
+        with open(str(Path(__file__).parent / "config.json")) as file:
+            config = json.load(file)
+            if "archives_path" not in config:
+                raise ValueError("archives_path not in config.json")
+            return Path(config['archives_path'])
